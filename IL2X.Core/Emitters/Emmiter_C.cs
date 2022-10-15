91bb9c222800dbcef6ed7bbe549d0ae123cc5e5b
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-10-15 14:51:20 -0700

    Fix compilation of builtins

--- IL2X.Core/Emitters/Emmiter_C.cs
+++ IL2X.Core/Emitters/Emmiter_C.cs
@@ -33,6 +33,88 @@ namespace IL2X.Core.Emitters
 		{
 			activeModule = module;
 
+			{
+				using var stream = new FileStream(Path.Combine(outputDirectory, "il2x_builtins.h"), FileMode.Create, FileAccess.Write, FileShare.Read);
+				using var writer = new StreamWriter(stream);
+
+				activeWriter = writer;
+				
+				WriteLine("#include <stdint.h>");
+				WriteLine();
+				WriteLine("#ifndef IL2X_BUILTINS_H");
+				WriteLine("/* === Safe Add/Subtract === */");
+				WriteLine(@"static inline int32_t IL2X_SafeAddInt32(int32_t a, int32_t b)
+{
+	if (b > 0)
+	{
+		if (a > INT32_MAX - b) return INT32_MAX; //TODO: make these proper exceptions, once they exist
+	}
+	else
+	{
+		if (a < INT32_MIN - b) return INT32_MIN;
+	}
+	return a + b;
+}");
+				WriteLine(@"static inline int32_t IL2X_SafeSubtractInt32(int32_t a, int32_t b)
+{
+	if (b > 0)
+	{
+		if (a < INT32_MIN + b) return INT32_MIN;
+	}
+	else
+	{
+		if (a > INT32_MAX + b) return INT32_MAX;
+	}
+	return a - b;
+}");
+				WriteLine(@"static inline int64_t IL2X_SafeAddInt64(int64_t a, int64_t b)
+{
+	if (b > 0)
+	{
+		if (a > INT64_MAX - b) return INT64_MAX; //TODO: make these proper exceptions, once they exist
+	}
+	else
+	{
+		if (a < INT64_MIN - b) return INT64_MIN;
+	}
+	return a + b;
+}");
+				WriteLine(@"static inline int64_t IL2X_SafeSubtractInt64(int64_t a, int64_t b)
+{
+	if (b > 0)
+	{
+		if (a < INT64_MIN + b) return INT64_MIN;
+	}
+	else
+	{
+		if (a > INT64_MAX + b) return INT64_MAX;
+	}
+	return a - b;
+}");
+				WriteLine(@"static inline uint32_t IL2X_SafeAddUInt32(uint32_t a, uint32_t b)
+{
+	if (UINT32_MAX - a < b) return UINT32_MAX; //TODO: make these proper exceptions, once they exist
+	return a + b;
+}");
+				WriteLine(@"static inline uint32_t IL2X_SafeSubtractUInt32(uint32_t a, uint32_t b)
+{
+	if (a < b) return 0;
+	return a - b;
+}");
+				WriteLine(@"static inline uint64_t IL2X_SafeAddUInt64(uint64_t a, uint64_t b)
+{
+	if (UINT64_MAX - a < b) return UINT64_MAX; //TODO: make these proper exceptions, once they exist
+	return a + b;
+}");
+				WriteLine(@"static inline uint64_t IL2X_SafeSubtractUInt64(uint64_t a, uint64_t b)
+{
+	if (a < b) return 0;
+	return a - b;
+}");
+				WriteLine("#endif");
+				WriteLine("#define IL2X_BUILTINS_H 1");
+			}
+			
 			// write header forward-declare all types
 			{
 				using (var stream = new FileStream(Path.Combine(outputDirectory, "__ForwardDeclares.h"), FileMode.Create, FileAccess.Write, FileShare.Read))
@@ -115,78 +197,8 @@ namespace IL2X.Core.Emitters
 						WriteLine("};");
                     }
 					WriteLine();
-
-					// write safe add and subtract functions
-					WriteLine("/* === Safe Add/Subtract === */");
-					WriteLine("#include <stdint.h>");
-					WriteLine(@"static inline int32_t IL2X_SafeAddInt32(int32_t a, int32_t b)
-{
-	if (b > 0)
-	{
-		if (a > INT32_MAX - b) return INT32_MAX; //TODO: make these proper exceptions, once they exist
-	}
-	else
-	{
-		if (a < INT32_MIN - b) return INT32_MIN;
-	}
-	return a + b;
-}");
-					WriteLine(@"static inline int32_t IL2X_SafeSubtractInt32(int32_t a, int32_t b)
-{
-	if (b > 0)
-	{
-		if (a < INT32_MIN + b) return INT32_MIN;
-	}
-	else
-	{
-		if (a > INT32_MAX + b) return INT32_MAX;
-	}
-	return a - b;
-}");
-					WriteLine(@"static inline int64_t IL2X_SafeAddInt64(int64_t a, int64_t b)
-{
-	if (b > 0)
-	{
-		if (a > INT64_MAX - b) return INT64_MAX; //TODO: make these proper exceptions, once they exist
-	}
-	else
-	{
-		if (a < INT64_MIN - b) return INT64_MIN;
-	}
-	return a + b;
-}");
-					WriteLine(@"static inline int64_t IL2X_SafeSubtractInt64(int64_t a, int64_t b)
-{
-	if (b > 0)
-	{
-		if (a < INT64_MIN + b) return INT64_MIN;
-	}
-	else
-	{
-		if (a > INT64_MAX + b) return INT64_MAX;
-	}
-	return a - b;
-}");
-					WriteLine(@"static inline uint32_t IL2X_SafeAddUInt32(uint32_t a, uint32_t b)
-{
-	if (UINT32_MAX - a < b) return UINT32_MAX; //TODO: make these proper exceptions, once they exist
-	return a + b;
-}");
-					WriteLine(@"static inline uint32_t IL2X_SafeSubtractUInt32(uint32_t a, uint32_t b)
-{
-	if (a < b) return 0;
-	return a - b;
-}");
-					WriteLine(@"static inline uint64_t IL2X_SafeAddUInt64(uint64_t a, uint64_t b)
-{
-	if (UINT64_MAX - a < b) return UINT64_MAX; //TODO: make these proper exceptions, once they exist
-	return a + b;
-}");
-					WriteLine(@"static inline uint64_t IL2X_SafeSubtractUInt64(uint64_t a, uint64_t b)
-{
-	if (a < b) return 0;
-	return a - b;
-}");
+					
+					WriteLine("#include \"il2x_builtins.h\"");
 				}
 			}
 
